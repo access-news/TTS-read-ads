7abb47f54e42c7528a539eb4f58d90e0f4edf7d4
Attila Gulyas <toraritte@gmail.com>
2022-05-08 20:39:37 -0400

    save

--- ads/lib/ads.ex
+++ ads/lib/ads.ex
@@ -33,83 +33,152 @@ defmodule Ads do
 
   def request(conn, path, headers) do
     Mint.HTTP.request(conn, "GET", path, headers, "")
-    # -> {:ok, conn, request_ref} where `request_ref` identifies responses in the output of `Mint.HTTP.stream/2` 
+    # -> {:error, conn, reason}
+    # -> {:ok, conn, request_ref}
+    #
+    #    where `request_ref` identifies responses returned by
+    #    `Mint.HTTP.stream/2`  (i.e., after  the request  has
+    #    been issued, messages from the request's destination
+    #    will come into  the mailbox of the calling - in this
+    #    case, this - process, which will become the argument
+    #    of `stream/2`)
   end
 
-  def safeway_parse_publication_list(jsonRaw) do
-    {:ok, json} = Jason.decode(jsonRaw)
+  # `json` here is usually a list because Safeway has 2 active publications at any given time: the weekly ads and Big Book of Savings. With that said, this will crash if this is not caseand the input JSON is not a list.
+  def safeway_parse_publication_list(json) do
     Enum.map(json, &(Map.get(&1, "id")))
   end
 
-  def f(conn) do
+  def safeway_get_publications(conn) do
+    {:ok, conn, request_ref} =
+      conn
+      |> safeway_connect()
+      |> safeway_publication_list()
+
+    {conn, aggregatedResponses} = Ads.collect_responses(conn)
+
+    publication_list_ids =
+      aggregatedResponses
+      |> stream_data_by_request_to_json(request_ref)
+      |> safeway_parse_publication_list()
+      |> Enum.map(&(safeway_publication(conn, &1)))
+    NOT DONE!
+  end
+
+  def collect_responses(conn) do
+    collect_responses(conn, [])
+  end
+
+  # This will always empty the the process' message box. (TODO Make sure)
+  def collect_responses(conn, aggregatedResponses) do
+
+      # For HTTPS,  `message` usually has the  below format,
+      # but not  important at this  point as it  is entirely
+      # pushed through `Mint.HTTP.stream/2` right away.
+      #
+      # { :ssl
+      # , { :sslsocket
+      #   , {:gen_tcp, #Port<0.7>, :tls_connection, :undefined}
+      #   , [#PID<0.235.0>, #PID<0.234.0>]
+      #   }
+      # , <<...>>
+      # }
+
     receive do
-
-      # {:ssl, _, _} = ssl ->
-      #   IO.inspect(ssl, label: :ssl)
-      #   conn
-
       message ->
         IO.puts("\n")
-        IO.puts("=== ENTER =================================================")
+        IO.puts("=== MESSAGE ===============================================")
         IO.inspect(message, label: :message)
         IO.puts("\n")
 
-        case Mint.HTTP.stream(conn, message) do
-          {:ok, conn, responses} = mintStream ->
-            IO.puts("\n")
-            IO.puts("=== :ok MINT STREAM =======================================")
-            IO.inspect(mintStream, label: :mintStream)
-            IO.puts("\n")
-            # IO.inspect(conn, label: :conn)
-            # IO.inspect(responses, label: :responses)
-            case Enum.filter(responses, &(elem(&1,0) === :data)) do
-              [] ->
-                IO.puts("\n")
-                IO.puts("=== BUMMBER [] ============================================")
-                IO.inspect(mintStream, label: :mintStream)
-                IO.puts("\n")
-                f(conn)
-              [{:data, _ref, zippedJSON} | _] = flyerData ->
-                IO.puts("\n")
-                IO.puts("=== :data BRANCH ==========================================")
-                IO.inspect(mintStream, label: :mintStream)
-                IO.puts("   ========================================================")
-                IO.puts("length(flyerData): #{length(flyerData)}")
-                IO.puts("   ========================================================")
-                IO.inspect(flyerData, label: :flyerData)
-                IO.puts("\n")
-                # 1. the actual flyer is loooong so recursively check
-                #    for ":done" (right? Not sure what it is supposed
-                #    to look like; check Mint.request/5 docs.)
-                # 2. move this json parsing to the :done branch
-                jsonRaw =
-                  zippedJSON
-                  |> :zlib.gunzip()
-                  |> IO.inspect(label: :json)
-                {conn, jsonRaw}
-            end
-
-          {:error, conn, reason, responses} ->
-            IO.puts("\n")
-            IO.puts("=== :error MINT STREAM ====================================")
-            IO.inspect(other, label: :error)
-            IO.puts("\n")
-            f(conn)
+        IO.puts("=== Mint.HTTP.stream/2 ====================================")
+        case (Mint.HTTP.stream(conn, message) |> tap(&IO.inspect/1)) do
+          # -> :unknown
+          #    a message not from the connection's socket
+          #
+          # -> {:error, conn, mintError, responsesList}
+          #    `responsesList`  is  "a  list  of  responses  that  were
+          #    correctly parsed before the error occured";
+          #    see possible responses below
+          #
+          # -> {:ok, conn, responsesList}
 
           :unknown ->
             IO.puts("\n")
             IO.puts("=== :unknown MINT STREAM ==================================")
             IO.puts("\n")
-            f(conn)
+            collect_responses(conn, aggregatedResponses)
+
+          {:error, conn, mintError, responsesList} ->
+            IO.puts("\n")
+            IO.puts("=== :error MINT STREAM ====================================")
+            IO.inspect(mintError, label: :error)
+            IO.puts("\n")
+            collect_responses(conn, aggregatedResponses ++ responsesList)
+
+          {:ok, conn, responsesList} = mintStream ->
+            IO.puts("\n")
+            IO.puts("=== :ok MINT STREAM =======================================")
+            IO.inspect(mintStream, label: :mintStream)
+            IO.puts("\n")
+            collect_responses(conn, aggregatedResponses ++ responsesList)
         end
     after
       0 ->
+        # No messages at all or finished aggregating all the responses
         IO.puts("=== after BRANCH ==========================================")
-        {:no_data, conn}
+        {conn, aggregatedResponses}
+    end
+  end
+
+  def stream_data_by_request_to_json(aggregatedResponses, request_ref) do
+
+    # Responses in `aggregatedResponses` have the following
+    # form (from the `Mint.HTTP.stream/2` docs):
+    #
+    # + {:status,       request_ref, status_code                  }
+    # + {:headers,      request_ref, headers                      }
+    # + {:data,         request_ref, binary                       }
+    # + {:done,         request_ref                               }
+    # + {:error,        request_ref, reason                       }
+    # + {:pong,         request_ref                               } HTTP/2 only
+    # + {:push_promise, request_ref, promised_request_ref, headers} HTTP/2 only
+    #
+
+    IO.puts("\n")
+    IO.inspect(aggregatedResponses, label: :all_responses)
+    IO.puts("\n")
+
+    # We only care about the `:data` right now:
+
+    dataResponses =
+      aggregatedResponses
+      |> Enum.filter(&(elem(&1,0) === :data))
+      |> Enum.filter(&(elem(&1,1) === request_ref))
+
+    case dataResponses do
+
+      [] ->
+        IO.puts("\n")
+        IO.puts("=== BUMMBER [] ============================================")
+        IO.puts("\n")
+        ""
+
+      # The Safeway flyer's data is gzipped (see response headers) so they will need to be unzipped before use.
+      # TODO Safeway uses Flipp, a couple other chains do too, but not sure about the rest.
+      [{:data, ^request_ref, _zippedJSON} | _] = flyerData ->
+
+        IO.puts("\n")
+        IO.puts("=== :data BRANCH ==========================================")
+        IO.puts("\n")
+
+        flyerData
+        |> Enum.reduce("", &(&2 <> elem(&1,2)))
+        |> :zlib.gunzip()
+        |> Jason.decode!()
     end
   end
 end
-
 # {:ok, conn} = Mint.HTTP.connect(:https, "dam.flippenterprise.net", 443 )
 # {:ok, conn, req_ref} = Mint.HTTP.request(conn, "GET", "/flyerkit/publications/safeway?locale=en&access_token=7749fa974b9869e8f57606ac9477decf&store_code=654", [], "")
 # receive do: (msg -> {:ok, conn, responses} = Mint.HTTP.stream(conn, msg))
